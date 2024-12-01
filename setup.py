8f6e80ad2cf41e584dc46b1f0fb5617327f50d3e
Fabian Köll <fabian.koell@enio.at>
2024-12-01 15:10:27 +0100

    0.1.1
    wip release to pypi

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
 from setuptools import setup, find_packages
 
+with open("README.rst", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="orbitalis",
-    version="0.1.0",
-    author="Your Name",
-    description="A library to approximate positions of planets and moons in the Solar System.",
-    long_description=open("README.rst").read(),
-    long_description_content_type="text/x-rst",
+    version="0.1.1",
+    author="Fabian Köll",
+    description="A library to approximate positions of planets and moons in our Solar System.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/koell/orbitalis",
-    packages=find_packages(),
-    license="GPLv3",
+    packages=find_packages(where="src"),
     install_requires=[],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.8',
 )
